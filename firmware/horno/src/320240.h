8e8a2e30be463f3ea2c29ec609058915edaa2f36
Elián Hanisch <lambdae2@gmail.com>
2016-09-21 22:24:54 -0300

    el encabezado 320240.h estaba comentado totalmente.
    a veces no entiendo como esto antes estaba funcionando.

--- firmware/horno/src/320240.h
+++ firmware/horno/src/320240.h
@@ -4,11 +4,11 @@
  *  Created on: 9 de ago. de 2016
  *      Author: Gastón Riera, Elián Hanisch y Rodrigo Oliver
  */
-/*
-#ifndef 320240_H_
-#define 320240_H_
+
+#ifndef D320240_H_
+#define D320240_H_
 
 void Horno_Display_Init(void);
 void Horno_Display_Test(void);
 
-#endif /* 320240_H_ */
+#endif /* D320240_H_ */
