7570d42d38128dca791cf48e60182297e7cc0248
Elián Hanisch <lambdae2@gmail.com>
2016-10-04 00:14:28 -0300

    actualizar el script para que grafique los 2 AD

--- octave/read_serial_continuo.m
+++ octave/read_serial_continuo.m
@@ -4,9 +4,11 @@ serial_init
 
 % creamos una figura
 clf
-clear t v
+clear t v w
 t(1) = 0; v(1) = 0;
-h = plot(t, v, '.');
+h1 = plot(t, v, 'xb');
+hold on
+h2 = plot(t, v, '+r');
 grid on
 
 log = fopen("registro.log", "a");
@@ -17,18 +19,21 @@ Tm = 1; % en este modo el muestreo es cada seg
 i = 1;
 unwind_protect
   while(1)
-    % leemos una línea, que son 18 bytes en total
-    str = char(srl_read(fd, 18));
-    n = base2dec(str(1:10), 10);
-    m = base2dec(str(13:16), 10);
-    if (isnan(n) || isnan(m))
+    % leemos una línea, que son 24 bytes en total
+    str = char(srl_read(fd, 24));
+    n = base2dec(str(1:10), 10);    % num. de muestra
+    d1 = base2dec(str(13:16), 10);  % termocupla
+    d2 = base2dec(str(19:22), 10);  % lm35
+    if (isnan(n) || isnan(d1) || isnan(d2))
       printf("error en la lectura de datos\n");
       break;
     end
     t(i) = n * Tm;
-    v(i) = m * LSB;
+    v(i) = d1 * LSB;
+    w(i) = d2 * LSB;
     % actualizar gráfico
-    set(h, 'XData', t, 'YData', v);
+    set(h1, 'XData', t, 'YData', v);
+    set(h2, 'XData', t, 'YData', w);
     drawnow;
     %guardamos un registro
     fwrite(log, str);
@@ -44,12 +49,11 @@ unwind_protect_cleanup
   fwrite(log, sprintf("-Fin de la captura %s\r\n", fecha()));
   fclose(log);
   % guardar los datos capturados
-  if (length(t) != length(v))
-    % antes de guardar las longitudes tienen que ser iguales
-    N = min([length(t), length(v)]);
-    t = t(1:N);
-    v = v(1:N);
-  end
-  csvwrite(sprintf("captura_continua_%s.csv", fecha()), [t' v']);
+  % antes de guardar las longitudes tienen que ser iguales
+  N = min([length(t), length(v), length(w)]);
+  t = t(1:N);
+  v = v(1:N);
+  w = w(1:N);
+  csvwrite(sprintf("captura_continua_%s.csv", fecha()), [t' v' w']);
   printf("captura guardada\n")
 end
