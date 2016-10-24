50f977a55048e4f311c44a2aac8ee71aca056c54
Rodrigo <blue_mass@hotmail.com>
2016-10-23 21:44:51 -0300

    Se agregan cosas que vienen de horno.c

--- firmware/horno/inc/adc.h
+++ firmware/horno/inc/adc.h
@@ -1,3 +1,26 @@
+/*
+ * delay.h
+ *
+ *  Created on: 27 de ago. de 2016
+ *      Author: m4v
+ */
+
+#ifndef ADC_H
+#define ADC_H
+
+#include "board.h"
+#include "init.h"
+
+
+
+#define ADC_CHANNEL ADC_TH  // canal de captura del comando 'm'
+#define NUM_MUESTRAS_CAPTURA 100
+#define NUM_MUESTRAS_ADC 1000*PERIODO_PROMEDIO/PERIODO_MUESTREO
+static bool adc_enabled = false;
+static bool adc_continue = false;
+static uint16_t muestras[NUM_MUESTRAS_CAPTURA];
+
+
 typedef struct {
 	uint32_t th_suma;			// suma accumulada del valor del AD (para hacer el promedio)
 	uint32_t th_cantidad;       // cantidad de valores sumados
@@ -10,6 +33,7 @@ typedef struct {
 	float    th_valor_c;
 	float    lm_valor_v;
 	float 	 lm_valor_c;
+	float 	 valor_c;
 } HORNO_PROMEDIO_T;
 
 static HORNO_PROMEDIO_T horno_adc;
@@ -19,3 +43,6 @@ void Horno_th_lineseg2(void);
 void Horno_th_lineseg3(void);
 void Horno_th_adctomv(void);
 void Horno_lm_line(void);
+
+
+#endif /* ADC_H_ */
