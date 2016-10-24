6c7049c3858d2d3071f9336222fef849b9310198
Rodrigo <blue_mass@hotmail.com>
2016-10-23 21:44:02 -0300

    Entra el sistickhandler() que viene del horno.c

--- firmware/horno/src/adc.c
+++ firmware/horno/src/adc.c
@@ -13,6 +13,7 @@
 
 #include "adc.h"
 
+
 #define ADC_SIZE 4096		//tamaño del ADC
 #define ADC_REF  3.3		//tension de referencia de ADC
 #define TH_GAIN  80.2 		//ganancia de la etapa de termocupla
@@ -27,13 +28,14 @@ float a3=0.078896, b3=19.13545, c3=75.8309;					// a.x^2+b.x+d
 float a4=16.09, b4=-3.429325;	//coeficientes de lm35, a4.x+b
 
 
+
 void horno_th_line(void){
 	if (horno_adc.th_valor<1229){
-		horno_th_lineseg1();
+		Horno_th_lineseg1();
 	}else if (horno_adc.th_valor>TH_SEG1 && horno_adc.th_valor<TH_SEG2){
-		horno_th_lineseg2();
+		Horno_th_lineseg2();
 	}else if(horno_adc.th_valor>TH_SEG3 && horno_adc.th_valor<TH_SEG3){
-		horno_th_lineseg3();
+		Horno_th_lineseg3();
 	}
 }
 
@@ -58,4 +60,48 @@ void Horno_lm_line(void){
 		horno_adc.lm_valor_v=horno_adc.lm_valor*ADC_REF/(ADC_SIZE*LM_GAIN);
 		horno_adc.lm_valor_c=a4*horno_adc.lm_valor_v+b4;
 }
+
+
+/* rutina de interrupción del systick */
+void SysTick_Handler(void)
+{
+	if (adc_enabled) {
+		if (!adc_continue) {
+			if (horno_adc.valor_n > NUM_MUESTRAS_CAPTURA) {
+				adc_enabled = false;
+				Board_LED_Set(0, false);
+			}
+			Chip_ADC_SetStartMode(LPC_ADC, ADC_START_NOW, ADC_TRIGGERMODE_RISING);
+			if(Chip_ADC_ReadStatus(LPC_ADC, ADC_CHANNEL, ADC_DR_DONE_STAT) == SET) {
+				Chip_ADC_ReadValue(LPC_ADC, ADC_CHANNEL, &muestras[horno_adc.valor_n++]);
+			}
+		} else {
+			/* captura continua */
+			if(Chip_ADC_ReadStatus(LPC_ADC, ADC_LM35, ADC_DR_DONE_STAT) == SET) {
+				uint16_t muestra;
+				Chip_ADC_ReadValue(LPC_ADC, ADC_TH, &muestra);
+				horno_adc.th_suma += muestra;
+				horno_adc.th_cantidad++;
+				Chip_ADC_ReadValue(LPC_ADC, ADC_LM35, &muestra);
+				horno_adc.lm_suma += muestra;
+				horno_adc.lm_cantidad++;
+				if (horno_adc.lm_cantidad >= NUM_MUESTRAS_ADC) {
+					Board_LED_Toggle(0);
+					horno_adc.th_valor = horno_adc.th_suma / horno_adc.th_cantidad;
+					horno_adc.lm_valor = horno_adc.lm_suma / horno_adc.lm_cantidad;
+					DEBUGOUT("%10d, %4d, %4d\r\n", horno_adc.valor_n,
+							                       horno_adc.th_valor,
+							                       horno_adc.lm_valor);
+					horno_adc.th_suma = 0;
+					horno_adc.th_cantidad = 0;
+					horno_adc.lm_suma = 0;
+					horno_adc.lm_cantidad = 0;
+					horno_adc.valor_n++;
+				}
+			}
+		}
+	}
+}
+
+
 
