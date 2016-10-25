4a01d2d142ccbadae18829485626b7c694092a18
Elián Hanisch <lambdae2@gmail.com>
2016-10-25 11:15:00 -0300

    configurar el ciclo de trabajo del PWM desde el teclado

--- firmware/horno/src/teclado.c
+++ firmware/horno/src/teclado.c
@@ -5,6 +5,7 @@
 #endif
 
 #include "delay.h"
+#include "pwm.h"
 
 #define _LPC_TIMER         LPC_TIMER0
 #define _TIMER_IRQn        TIMER0_IRQn
@@ -47,54 +48,82 @@
 #define COL3  GETCOL3(Chip_GPIO_GetPortValue(LPC_GPIO, 0))
 #define COL4  GETCOL4(Chip_GPIO_GetPortValue(LPC_GPIO, 0))
 
+typedef struct {
+	uint32_t dato;
+} HORNO_TECLADO_T;
+
+static HORNO_TECLADO_T horno_keypad = { 0 };
+
 static uint32_t counter;
 
 void TECLA1_Handler(void) {
 	DEBUGOUT("1");
+	horno_keypad.dato *= 10;
+	horno_keypad.dato += 1;
 }
 
 void TECLA2_Handler(void) {
 	DEBUGOUT("2");
+	horno_keypad.dato *= 10;
+	horno_keypad.dato += 2;
 }
 
 void TECLA3_Handler(void) {
 	DEBUGOUT("3");
+	horno_keypad.dato *= 10;
+	horno_keypad.dato += 3;
 }
 
 void TECLA4_Handler(void) {
 	DEBUGOUT("4");
+	horno_keypad.dato *= 10;
+	horno_keypad.dato += 4;
 }
 
 void TECLA5_Handler(void) {
 	DEBUGOUT("5");
+	horno_keypad.dato *= 10;
+	horno_keypad.dato += 5;
 }
 
 void TECLA6_Handler(void) {
 	DEBUGOUT("6");
+	horno_keypad.dato *= 10;
+	horno_keypad.dato += 6;
 }
 
 void TECLA7_Handler(void) {
 	DEBUGOUT("7");
+	horno_keypad.dato *= 10;
+	horno_keypad.dato += 7;
 }
 
 void TECLA8_Handler(void) {
 	DEBUGOUT("8");
+	horno_keypad.dato *= 10;
+	horno_keypad.dato += 8;
 }
 
 void TECLA9_Handler(void) {
 	DEBUGOUT("9");
+	horno_keypad.dato *= 10;
+	horno_keypad.dato += 9;
 }
 
 void TECLA0_Handler(void) {
 	DEBUGOUT("0");
+	horno_keypad.dato *= 10;
 }
 
 void TECLAA_Handler(void) {
 	DEBUGOUT("A");
+	Horno_pwm_ciclo(horno_pwm.dc);
+	Horno_pwm_inicio();
 }
 
 void TECLAB_Handler(void) {
 	DEBUGOUT("B");
+	Horno_pwm_parar();
 }
 
 void TECLAC_Handler(void) {
@@ -107,12 +136,16 @@ void TECLAD_Handler(void) {
 
 /* tecla asterisco */
 void TECLAE_Handler(void) {
-	DEBUGOUT("\b");
+	DEBUGOUT("\b"); /* \b -> backspace */
+	horno_keypad.dato /= 10;
 }
 
 /* tecla numeral */
 void TECLAF_Handler(void) {
 	DEBUGOUT("\n");
+	DEBUGOUT("dato ingresado: %d\n", horno_keypad.dato);
+	Horno_pwm_ciclo((float)(horno_keypad.dato)/100.0);
+	horno_keypad.dato = 0;
 }
 
 void COLUMN1_Handler(void) {
