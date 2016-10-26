1ebbc712e349361d1986bcd12b7db33159088050
Rodrigo <blue_mass@hotmail.com>
2016-10-25 22:45:40 -0300

    se cambio la definicion del PI Para que quede PI = Kp + Ki * Ts se cambio tambien nombres de variables de temperatura que se modificaron en control.h

--- firmware/horno/src/control.c
+++ firmware/horno/src/control.c
@@ -5,10 +5,10 @@
  *      Author: Nico
  */
 
-
-#define KP 5.23
-#define TAO 10098
-#define TS 0.1
+/* valores del PI */
+#define KP 5.23			// constante proporcional
+#define KI 9.9e-5		// constante integrador
+#define TS 0.1			// periodo de muestreo
 
 #include "control.h"
 #include "adc.h"
@@ -17,29 +17,30 @@
 
 
 /*
- * @brief se calcula el PI
+ * @brief se define PI discreto
+ *
+ * PI= Kp + Ki * Ts
  */
 void Horno_PI(void){
-	horno_pi.K= KP*TS;
-	horno_pi.Ti= 3.3*TAO;
-	horno_pi.I= TS/horno_pi.Ti;
+	horno_pi.Kp= KP;
+	horno_pi.Ki= KI*TS;
 }
 
 /*
  * @brief Lazo de control PI
  */
 void Horno_control_pi(void){
-	horno_control.Err= horno_control.temp_set - horno_adc.valor_c ; // calculo de error
-	horno_control.PI_K= horno_pi.K*horno_control.Err;				// calculo de termino proporcional
-	horno_control.PI_I= horno_pi.I*horno_control.Err;				// calculo de termino integrador
+	horno_control.Err= horno_control.temp_set - horno_adc.temperatura ; // calculo de error
+	horno_control.PI_K= horno_pi.Kp*horno_control.Err;				// calculo de termino proporcional
+	horno_control.PI_I= horno_pi.Ki*horno_control.Err;				// calculo de termino integrador
 	horno_control.PI_Out= horno_control.PI_K + horno_control.PI_I;	// calculo de salida de PI
 }
 /*
  * @brief Lazo de control P
  */
 void Horno_control_p(void){
-	horno_control.Err= horno_control.temp_set - horno_adc.valor_c; // calculo de error
-	horno_control.PI_Out= horno_pi.K*horno_control.Err;			   // calculo de termino proporcional
+	horno_control.Err= horno_control.temp_set - horno_adc.temperatura; // calculo de error
+	horno_control.PI_Out= horno_pi.Kp*horno_control.Err;			   // calculo de termino proporcional
 }
 
 
